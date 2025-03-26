4f9fd80fb5f8369fb24843aabd5b26167b7d53a3
Goegoecha <116905665+Goegoecha@users.noreply.github.com>
2025-03-26 22:49:32 +0530

    Update Footer.jsx
    updated facebook link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -53,7 +53,7 @@ function Footer() {
             <h4 className="text-lg font-semibold mb-6 text-gray-200">Follow Us</h4>
             <ul className="flex flex-wrap gap-4 justify-center">
               <li>
-                <a href='https://www.facebook.com/profile.php?id=61571194276624' target='_blank'>
+                <a href='https://www.facebook.com/profile.php?id=61571494110390' target='_blank'>
                   <svg xmlns="http://www.w3.org/2000/svg" className="fill-blue-600 w-8 h-8" viewBox="0 0 49.652 49.652">
                     <path d="M24.826 0C11.137 0 0 11.137 0 24.826c0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826C49.652 11.137 38.516 0 24.826 0zM31 25.7h-4.039v14.396h-5.985V25.7h-2.845v-5.088h2.845v-3.291c0-2.357 1.12-6.04 6.04-6.04l4.435.017v4.939h-3.219c-.524 0-1.269.262-1.269 1.386v2.99h4.56z" data-original="#000000" />
                   </svg>
